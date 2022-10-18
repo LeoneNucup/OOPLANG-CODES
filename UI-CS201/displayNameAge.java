57072b7098d0a12e616bcb5b5d379e006b96453e
LeoneNucup <nucupleone@gmail.com>
2022-10-18 12:57:55 +0800

    10/18/22

--- UI-CS201/displayNameAge.java
+++ UI-CS201/displayNameAge.java
@@ -15,17 +15,17 @@ public class displayNameAge extends JFrame {
     private void okButtonPressed(ActionEvent e) {
         JFrame f;
         f = new JFrame();
-        JOptionPane.showMessageDialog(f, "Name is : " + nameField.getText()+", and your Age is : " + ageField.getText());
+        JOptionPane.showMessageDialog(f,
+                "Name is : " + nameField.getText() + ", and your Age is : " + ageField.getText());
         System.exit(0);
     }
-    
+
     private void exitButtonPressed(ActionEvent e) {
         JFrame f;
         f = new JFrame();
-        JOptionPane.showMessageDialog(f,"Thank You for Using this Program!");
+        JOptionPane.showMessageDialog(f, "Thank You for Using this Program!");
         System.exit(0);
-        
-        
+
     }
 
     public displayNameAge() {
@@ -34,51 +34,48 @@ public class displayNameAge extends JFrame {
 
         getContentPane().setLayout(new GridBagLayout());
         GridBagConstraints gridConstraints = new GridBagConstraints();
-        
+
         nameLabel.setText("Name");
         gridConstraints.gridx = 0;
         gridConstraints.gridy = 0;
         getContentPane().add(nameLabel, gridConstraints);
-        
+
         ageLabel.setText("Age");
         gridConstraints.gridx = 0;
         gridConstraints.gridy = 1;
         getContentPane().add(ageLabel, gridConstraints);
-        
+
         nameField.setText("");
         nameField.setColumns(10);
         gridConstraints.gridx = 2;
         gridConstraints.gridy = 0;
-        getContentPane().add(nameField,gridConstraints);
-        
+        getContentPane().add(nameField, gridConstraints);
+
         ageField.setText("");
         ageField.setColumns(10);
         gridConstraints.gridx = 2;
         gridConstraints.gridy = 1;
-        getContentPane().add(ageField,gridConstraints);
-        
-        
+        getContentPane().add(ageField, gridConstraints);
+
         okButton.setText("OK");
         gridConstraints.gridx = 0;
         gridConstraints.gridy = 5;
         getContentPane().add(okButton, gridConstraints);
-        
+
         exitButton.setText("EXIT");
         gridConstraints.gridx = 2;
         gridConstraints.gridy = 5;
         getContentPane().add(exitButton, gridConstraints);
-        
-        
+
         okButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 okButtonPressed(e);
-                
+
             }
         });
-        
-        
+
         exitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 exitButtonPressed(e);
             }
         });
